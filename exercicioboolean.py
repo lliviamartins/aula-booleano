73386d0f238fe45ac3b3c83fb8d39ab09ab8cb47
lliviamartins <lliviasantospaz2019@gmail.com>
2026-03-05 09:28:56 -0300

    exercicios

---
+++ exercicioboolean.py
@@ -0,0 +1,21 @@
+a= False
+b= False
+regra = ((not a) and b) or ((not b) and a)
+print(a,b,regra)
+
+a = False
+b = True
+regra = ((not a) and b) or ((not b) and a)
+print (a,b,regra)
+
+a= True 
+b= False
+regra = ((not a) and b) or ((not b) and a)
+print(a,b,regra)
+
+a= True
+b= True
+regra = ((not a) and b) or ((not b) and a)
+print(a,b,regra)
+
+
---
+++ exercicioboolean2.py
@@ -0,0 +1,7 @@
+a = True
+b = True
+c = True
+d = True
+regra = (not(a and (not c)) or (b or d)) and ((not d) and c))
+print(a,b,c,d, regra)
+
